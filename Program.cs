59e269be43a1cf201e5f779860a92ef124f992bb
Андрей Кан <1kan_andreikkkk@mail.ru>
2021-03-10 20:02:38 +0600

    Last home 4

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _311020
+{
+    class Program
+    {
+        private int x;
+        private int y;
+        private int tcount;
+        private object label;
+
+        public object Convent { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        private void textBox1_Click(Object sender,EventArgs e)
+        {
+            TexBox t = new TexBox();
+            t.Parent = this;
+            t.Text = "Новое поле ввода";
+            x = x + 10;
+            y = y + 20;
+            tcount = tcount + 1;
+            label.Text = "Количество полей ввода=" + Convent.ToString(tcount);
+        }
+    }
+}
---
+++ TexBox.cs
@@ -0,0 +1,9 @@
+﻿namespace _311020
+{
+    internal class TexBox
+    {
+        internal Program Parent;
+
+        public string Text { get; internal set; }
+    }
+}
